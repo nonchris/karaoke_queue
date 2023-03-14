8386478c0617bf2218e22469831a47ca74dd57bd
nonchris <git@chris-ge.de>
2023-03-14 10:36:17 +0100

    Rename and remove references

--- src/karaoke_queue/__init__.py
+++ src/karaoke_queue/__init__.py
@@ -1,2 +1,1 @@
-from . import convert
 from .main import app
--- src/karaoke_queue/webserver.py
+++ src/karaoke_queue/webserver.py
@@ -8,7 +8,7 @@ def start():
     Start the webserver.
     Run the app with:
 
-    uvicorn whisper_api:app --reload --host 127.0.0.1 --port 3001
+    uvicorn karaoke_queue:app --reload --host 127.0.0.1 --port 3001
 
     access docs:
     http://127.0.0.1:3001/docs
