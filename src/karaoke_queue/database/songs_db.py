92439c7148290756accb38bb948a6c696766d24a
nonchris <git@chris-ge.de>
2023-03-14 14:12:46 +0100

    Import db_getters into songs_db

--- src/karaoke_queue/database/songs_db.py
+++ src/karaoke_queue/database/songs_db.py
@@ -1,6 +1,7 @@
 from .db_base import *
 from .db_writers import *
+from .db_getters import *
 
 if __name__ == '__main__':
     setup_db()
-    s = Song("Never gonna give you up", "rick astley", 6.9)
+    s = Song("Never gonna give you up", "rick astley", 6.9, 10)
