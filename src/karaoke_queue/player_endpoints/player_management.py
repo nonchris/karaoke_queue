bb8d8ec993d0deed88dc8f4760931eef10eb6486
nonchris <git@chris-ge.de>
2023-03-15 00:41:00 +0100

    Update join_room() to respond with a session cookie for player authentication

--- src/karaoke_queue/player_endpoints/player_management.py
+++ src/karaoke_queue/player_endpoints/player_management.py
@@ -11,5 +11,24 @@ room_manager = RoomManager()
 
 
 @router.get("/join/{room_id}")
-async def join_room(room_id: str, user_name):
+async def join_room(room_id: str, player_name):
+    """
+    Allows player to join a room by a selected name (must be unique!)
+    :param room_id: the room to join
+    :param player_name: Name the player wants to be displayed as
+    :return: Contains player & room info and session cookie to authenticate the user
+    """
     room = try_get_room(room_id)
+    player = room_manager.add_player(room.name, player_name)
+
+    content = {
+        "room": room.user_to_transmit_info,
+        "player": player.to_transmit,
+        "links": {
+            "queue_menu": f"{get_room_user_endpoint(room)}/queue_menu"
+        },
+    }
+    response = JSONResponse(content=content)
+    response.set_cookie(key="player_id", value=player.uuid)
+
+    return response
