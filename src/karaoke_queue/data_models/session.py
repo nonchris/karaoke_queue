7c800cabf103cef14a905c278eb1ae18d64259f1
nonchris <git@chris-ge.de>
2023-03-14 14:13:32 +0100

    Base SessionManager on QueueEntries and not Songs

--- src/karaoke_queue/data_models/session.py
+++ src/karaoke_queue/data_models/session.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from uuid import uuid4
 
 from .song import Song
+from .queue_entry import QueueEntry
 from .types import uuid_hex_t
 from .read_write_lock import ReadWriteLock, with_write_lock, with_read_lock
 
@@ -14,13 +15,14 @@ class Session:
     uuid: uuid_hex_t = uuid4().hex
 
     def __post_init__(self):
-        self.queue: deque[Song] = deque([], maxlen=self.max_queue_len)
+        self.__queue: deque[QueueEntry] = deque([], maxlen=self.max_queue_len)
         self.lock = ReadWriteLock()
 
     @with_read_lock
     def __queue_in_transmittable_format(self):
-        return [song.to_transmit for song in self.queue]
+        return [entry.to_transmit for entry in self.__queue]
 
+    @property
     @with_read_lock
     def admin_to_transmit_info(self) -> dict:
         to_transmit = {
@@ -30,6 +32,7 @@ class Session:
             }
         return to_transmit
 
+    @property
     @with_read_lock
     def user_to_transmit_info(self) -> dict:
         to_transmit = {
@@ -39,5 +42,5 @@ class Session:
         return to_transmit
 
     @with_write_lock
-    def add_song(self, song: Song):
-        self.queue.append(song)
+    def add_to_queue(self, song: Song, user: str = "Unknown"):
+        self.__queue.append(QueueEntry(song, user))
