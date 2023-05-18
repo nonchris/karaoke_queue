d46810fe4aeb943cfe7772263d7426eb38940c39
nonchris <git@chris-ge.de>
2023-05-18 20:45:37 +0200

    fix more imports

--- src/karaoke_queue/database/songs_db.py
+++ src/karaoke_queue/database/songs_db.py
@@ -1,6 +1,6 @@
-from .db_base import *
-from .db_writers import *
-from .db_getters import *
+from karaoke_queue.database.db_base import *
+from karaoke_queue.database.db_writers import *
+from karaoke_queue.database.db_getters import *
 
 if __name__ == '__main__':
     setup_db()
